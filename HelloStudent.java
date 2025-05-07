c24dd2c5bac0d0cf64b22e9c30514f3405b389aa
akashsonawane571 <akashsonawane571@gmail.com>
2025-05-08 05:22:28 +0530

    check remove  poll

--- HelloStudent.java
+++ HelloStudent.java
@@ -1,6 +1,7 @@
 public class HelloStudent {
     public static void main(String[] args) {
-        System.out.println("Hello, my name is [Your Name]");
+        System.out.println("Hello, my name is Akash Sonawane");
         System.out.println("Welcome to C-DAC");
+        System.out.println("Welcome to DITISS");
     }
 }
