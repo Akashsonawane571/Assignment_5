aafe9afe45221b3448652279a37ceeb3d77a6fde
akashsonawane571 <akashsonawane571@gmail.com>
2025-05-08 05:00:31 +0530

    Add welcome message

---
+++ HelloStudent.java
@@ -0,0 +1,6 @@
+public class HelloStudent {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Akash Sonawane");
+        System.out.println("Welcome to C-DAC");
+    }
+}
